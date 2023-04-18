a2932cba0217ad767a41e8c54c58fb3a159a341b
NicoBalbuena <112787278+NicoBalbuena@users.noreply.github.com>
2023-04-18 08:59:39 -0300

    #45
    Se modifica

--- JAVASCRIPT/TSLeccion02/02-03-arreglos.js
+++ JAVASCRIPT/TSLeccion02/02-03-arreglos.js
@@ -1,3 +1,9 @@
 //let autos = new Array('Ferrari','Renault','BMW'); Esta es la sintaxis vieja
 const autos = ['Ferrari','Renault','BMW'];
 console.log(autos);
+
+
+//Modificamos los elementos del arreglo
+autos[1]='Volvo';
+console.log(autos[1]);
+console.log(autos);
