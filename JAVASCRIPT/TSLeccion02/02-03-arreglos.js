0ff8746af100a13b58a126d2878f44e59402d50d
Juan Carlos Gutierrez <DiosJuanVero2015@gmail.com>
2023-04-20 01:10:52 -0300

    JAVASCRIPT-Recorrer los elementos de un Arreglo#44

--- JAVASCRIPT/TSLeccion02/02-03-arreglos.js
+++ JAVASCRIPT/TSLeccion02/02-03-arreglos.js
@@ -2,6 +2,13 @@
 const autos = ['Ferrari','Renault','BMW'];
 console.log(autos);
 
+//Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0;i < autos.length;i++){
+    console.log(i + " : " + autos[i]);
+}
 
 //Modificamos los elementos del arreglo
 autos[1]='Volvo';
