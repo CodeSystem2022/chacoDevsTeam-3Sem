49bae3a6707758616e8992cf7ac73d92bcd01eb4
NicoBalbuena <112787278+NicoBalbuena@users.noreply.github.com>
2023-04-21 22:53:38 -0300

    Funciones.js
    Se crea la funcion y se utiliza la palabra return

---
+++ JAVASCRIPT/TSLeccion03/01-03-funciones.js
@@ -0,0 +1,12 @@
+// Balbuena Fernando-Creamos una nueva funcion
+miFuncion(10,15);//Hosting
+function miFuncion(a,b){
+    console.log("Sumamos: "+ (a+b));
+    return a+b;
+    
+}
+//Llamando a la función
+miFuncion(5,4);
+let resultado=miFuncion(6,7);
+console.log(resultado);
+
