85e5d977794d8a7ddc9d60a427dc403fe35cc4a6
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-04-24 20:56:19 -0300

    TestEnumeraciones#94
    Metodo switch agregado.

--- JAVA/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
+++ JAVA/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -4,6 +4,21 @@ import enumeraciones.Dias;
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        System.out.println("Dia:"+Dias.LUNES);
+        System.out.println("Dia 1:"+Dias.LUNES);
+        indicarDiaSemana(Dias.LUNES);
+    }
+
+    private static void indicarDiaSemana(Dias dias){
+        switch(dias){
+                case LUNES:
+                System.out.println("Primer dia de la semana");
+                break;
+                case MARTES:
+                System.out.println("Segundo dia de la semana");
+                break;
+                default:
+                System.out.println("El valor ingresado no es valido")
+                        break;
+        }
     }
 }
