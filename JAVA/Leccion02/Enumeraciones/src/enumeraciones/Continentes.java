d26e351d4cd30426634ec5bd787efc73a25121e9
NicoBalbuena <112787278+NicoBalbuena@users.noreply.github.com>
2023-04-24 17:51:47 -0300

    Clase continente
    Se crea la clase continente

--- JAVA/Leccion02/Enumeraciones/src/enumeraciones/Continentes.java
+++ JAVA/Leccion02/Enumeraciones/src/enumeraciones/Continentes.java
@@ -1,11 +1,11 @@
-package test;
+package enumeraciones;
 
 public enum Continentes {
         AFRICA(53,"1.2 billones"),
         EUROPA(46,"1.1 billones"),
         ASIA(44,"1.9 billones"),
         AMERICA(34,"150.2 billones"),
-        OCEANIA(14,"1.2 billones");
+        OCEANTA(14,"1.2 billones");
 
         private final int paises;
         private String habitantes;
@@ -21,5 +21,4 @@ public enum Continentes {
         public String getHabitantes(){
             return this.habitantes;
         }
-
     }
