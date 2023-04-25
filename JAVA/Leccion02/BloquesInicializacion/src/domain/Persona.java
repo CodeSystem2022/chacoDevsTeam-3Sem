d7002d2b3c30faff2ac59b5b6421ca06a763d1bc
Federico65744 <cvaldez250@outlook.com>
2023-04-24 21:48:40 -0600

    Agregado tiquet #97 y #98

---
+++ JAVA/Leccion02/BloquesInicializacion/src/domain/Persona.java
@@ -0,0 +1,26 @@
+package domain;
+
+public class Persona {
+    private final int idPersona;
+    private static int contadorPersonas;
+    
+    static{//Bloque de Inicialización estático
+        System.out.println("Ejecución del bloque estatico");
+        ++Persona.contadorPersonas;
+        //idPersona = 10; No es estatico un atributo, por esto tenemos un error
+    }
+    
+    {//Bloque de inicialización No estático(contexto dinámico)
+        System.out.println("Ejecución del bloque no estático");
+        this.idPersona = Persona.contadorPersonas++;//Incrementamos el atributo
+    }
+    
+    //Los bloques de inicialización se ejecutan antes del contructor
+    public Persona(){
+        System.out.println("Ejecución del contructor");
+    }
+    
+    public int getIdPersona(){
+        return this.idPersona;
+    }
+}
