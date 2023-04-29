4e53ebfeebcba7cc71c9c5bf200197083cea1d6c
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2023-04-29 14:04:15 -0300

    Update TestAutoboxinUnboxing.java
    Se agregan lineas que faltaban

--- JAVA/Leccion03/AutoBoxinUnboxin/src/test/TestAutoboxinUnboxing.java
+++ JAVA/Leccion03/AutoBoxinUnboxin/src/test/TestAutoboxinUnboxing.java
@@ -2,10 +2,17 @@ package test;
 
 public class TestAutoboxinUnboxing {
     public static void main(String[] args){
-        //Clases Envolventes o Wrapper
+       //Clases envolventes o Wrapper
         /*
-           Clases envolventes de tipos primitivos
-            int = la clase envolvente es integer
+          Clases envolventes de tipos primitivos 
+          1-int = la clase envolvente es Integer
+          2-long = la clase envolvente es Long
+          3-float = la clase envolvente es Float
+          4-double = la clase envolvente es Double
+          5-boolean = la clase envolvente es Boolean
+          6-byte = la clase envolvente es Byte
+          7-char = la clase envolvente es Character
+          8-short = la clase envolvente Short
         */
         
         int enteroPrim = 10; //Tipo primitivo
