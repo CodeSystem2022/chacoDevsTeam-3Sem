2eb3f19a5c28a1291bd437dc16cfde409e7c731b
JuanaNataliaGutierrez <naty.rcia33@gmail.com>
2023-05-12 23:23:12 -0300

    Agrego constructores en chacoDevsTeam-3Semestre#172

--- JAVASCRIPT/TSLeccion04/01-04.Objetos.js
+++ JAVASCRIPT/TSLeccion04/01-04.Objetos.js
@@ -62,3 +62,16 @@ console.log(personaArray);
 console.log('Distintas formas de imprimir un objeto: forma 4');
 let personaString = JSON.stringify(persona);
 console.log(personaString);
+
+
+function Persona3(nombre, apellido, email){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+}
+let padre = new Persona3('Leo', 'Lopez', 'lopezl@gmail.com');
+padre.nombre = 'Luis';
+console.log(padre); 
+
+let madre = new Persona3('Laura', 'Contrera', 'contreral@gmail.com');
+console.log(madre);
