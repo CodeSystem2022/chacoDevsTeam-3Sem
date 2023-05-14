3a019f1a93e2ccee72666cbcbd06aa885ae4d85f
Alejo Gutierrez <alejohgutierrez@hotmail.com>
2023-05-14 12:51:02 -0300

    JS-AGREGAR METODOS AL CONSTRUCTOR DEL OBJETO#173

--- JAVASCRIPT/TSLeccion04/01-04.Objetos.js
+++ JAVASCRIPT/TSLeccion04/01-04.Objetos.js
@@ -77,13 +77,18 @@ function Persona3(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre+' '+this.apellido;
+    }    
 }
 let padre = new Persona3('Leo', 'Lopez', 'lopezl@gmail.com');
-padre.nombre = 'Luis';
+padre.nombre = 'Luis'; // modificamos el nombre 
 padre.telefono = '549261828221';
 console.log(padre);
+console.log(padre.nombreCompleto()); //Utilizamos la funcion
 let madre = new Persona3('Laura', 'Contrera', 'contreral@gmail.com');
 console.log(madre);
+console.log(madre.nombreCompleto());
 console.log(madre.telefono); //la propiedad no está definida
 
 console.log('Comenzamos a utilizar el metodo get');
