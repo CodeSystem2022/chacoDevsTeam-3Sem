f04b63e573f507459387b1c2bd7876a4616f2c1a
JuanValladaresv2 <juan_1@outlook.es>
2023-05-18 21:47:57 -0300

    test TICKET #218
    Se requiere crear geter and seter del atributo apellido, usar console.log para visualizar su estado y aplicar el concepto a clases (Tarea - VIDEO 5 y 6)

--- JAVASCRIPT/TSLeccion05/05-03-clases.js
+++ JAVASCRIPT/TSLeccion05/05-03-clases.js
@@ -1,13 +1,30 @@
+//Video 5 Valladares Juan Ignacio
+//let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer: Persona is not defined
+
 class Persona{
     constructor(nombre, apellido){
         this.persona = nombre;
         this.apellido = apellido;
     }
+
+
+//Video 6 Valladares Juan Ignacio
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
+
 let persona1 =new Persona('Martin', 'Perez');
+console.log(persona1.apellido);
 console.log(persona1);
 let persona2 =new Persona('Martin', 'Perez');
+console.log(persona2.apellido)
 console.log(persona2);
+
 
 //video 7
 //Espinola Renzo Oscar Alejo
