8f28470e3b579366dc54d34c0585cd30338571ed
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2023-05-26 23:25:23 -0300

    Update TestInterfaces.java

--- JAVA/Leccion06/Interfaces/src/test/TestInterfaces.java
+++ JAVA/Leccion06/Interfaces/src/test/TestInterfaces.java
@@ -4,18 +4,16 @@ import accesodatos.IAccesoDatos;
 import accesodatos.ImplementacionMySql;
 import accesodatos.ImplementacionOracle;
 
-//video 5
+
 public class TestInterfaces {
     public static void main(String[] args) {
     IAccesoDatos datos = new ImplementacionMySql();
     //datos.listar();
-    //clase 6
     //imprimir(datos);
     datos = new ImplementacionOracle();
     //datos.listar();
     imprimir(datos);
     }
-    //clase 6
     public static void imprimir(IAccesoDatos datos){
         datos.listar();
     }
