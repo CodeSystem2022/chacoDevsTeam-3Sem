2ef412834c6c80dcd9933cf23fe3dc47adee9e54
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-06-03 00:58:04 -0300

    PYTHON 2 ETAPA-Capa UI-ui.py
    Se creo la clase ui.py y se modificó el diseño para la ejecución por consola.

---
+++ PYTHON/TrabajoFinal3Semestre/GestionBiblioteca/UI/ui.py
@@ -0,0 +1,63 @@
+def mostrar_menu_socio():
+    print("************************************")
+    print('             ABM SOCIO              ')
+    print("************************************")
+    print('1.--> Agregar Socio')
+    print('2.--> Listar Socios')
+    print('3.--> Eliminar un socio')
+    print('4.--> Modificar un socio')
+    print('5.--> Volver')
+    print("************************************")
+
+def mostrar_menu_autor():
+    print("************************************")
+    print('             ABM AUTOR              ')
+    print("************************************")
+    print("1.--> Agregar autor")
+    print('2.--> Listar autores')
+    print('3.--> Eliminar un autor')
+    print('4.--> Modificar un autor')
+    print('5.--> Volver')
+    print("************************************")
+
+def mostrar_menu_libro():
+    print("************************************")
+    print('            ABM de Libro            ')
+    print("************************************")
+    print("1.--> Agregar un libro")
+    print('2.--> Listar libros')
+    print('3.--> Eliminar un libro')
+    print('4.--> Modificar un libro')
+    print('5.--> Agregar un libro existente')
+    print('6.--> Volver')
+    print("************************************")
+
+def mostrar_menu_principal_abm1():
+    print("************************************")
+    print('         GESTION BIBLIOTECA         ')
+    print("************************************")
+    print("1.--> ABM SOCIO")
+    print('2.--> ABM AUTOR')
+    print('3.--> ABM LIBROS')
+    print('4.--> Volver')
+    print("************************************")
+
+def mostrar_menu_principal():
+    print("*******************************************")
+    print('            GESTION BIBLIOTECA             ')
+    print("*******************************************")
+    print("1.--> Administración de Socio, Autor, Libro")
+    print("2.--> Retiro/Devolución libros")
+    print('3.--> Salir')
+    print("*******************************************")
+
+def mostrar_menu_solicitud():
+    print("************************************")
+    print('         GESTION BIBLIOTECA         ')
+    print("************************************")
+    print("1.--> Solicitud de libro")
+    print("2.--> Devolución libros")
+    print('3.--> VOLVER')
+    print("************************************")
+
+mostrar_menu_libro()
