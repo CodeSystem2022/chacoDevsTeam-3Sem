0d932101150837dbb56ec79711927c40f8669d27
Renzo Espinola <renzo.espinola87@gmail.com>
2023-06-06 22:31:38 -0300

    hotFix sobre persona_dao.py

--- PYTHON/Leccion07/capa_datos_persona/persona_dao.py
+++ PYTHON/Leccion07/capa_datos_persona/persona_dao.py
@@ -1,5 +1,5 @@
-from capa_datos_persona.Persona import Persona
-from capa_datos_persona.conexion import Conexion
+from Persona import Persona
+from conexion import Conexion
 from logger_base import log
 
 
@@ -16,22 +16,23 @@ class PersonaDAO:
     _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
     _INSERTAR = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s,%s,%s)'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-    _ELIMINAR ='DELETE FROM persona WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
 
     # Definimos los metodos de clase
     @classmethod
     def seleccionar(cls):
         with Conexion.obtenerConexion():
-            with Conexion.obtenerCursor(): as cursor:
+            with Conexion.obtenerCursor() as cursor:
                 cursor.execute(cls._SELECCIONAR)
                 registros = cursor.fetchall()
-                persona = [] #creamos una lista
+                personas = []  # creamos una lista
                 for registro in registros:
-                    persona = Persona(registros[0], registros[1], registros[2], registros[3])
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
                     personas.append(persona)
                 return personas
 
-if __name__ == '__main__'
+
+if __name__ == '__main__':
     personas = PersonaDAO.seleccionar()
     for persona in personas:
         log.debug(persona)
