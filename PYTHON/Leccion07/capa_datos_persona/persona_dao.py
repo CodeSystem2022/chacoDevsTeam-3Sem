4a26e9398158f236d125b031cf6c453264b694a7
Tatiana Tamara Gimenez Rios <tatagimeri07@gmail.com>
2023-06-06 23:11:27 -0300

    PYTHON-Metodo de clase Eliminar#312

--- PYTHON/Leccion07/capa_datos_persona/persona_dao.py
+++ PYTHON/Leccion07/capa_datos_persona/persona_dao.py
@@ -31,8 +31,23 @@ class PersonaDAO:
                     personas.append(persona)
                 return personas
 
+    @classmethod
+    def eliminar(cls,persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Los objetos eliminados  son: {persona}')
+                return cursor.rowcount
+
 
 if __name__ == '__main__':
+    # Eliminar un registro
+    # persona1 = Persona(id_persona=8)
+    # personas_eliminadas = PersonaDAO.eliminar(persona1)
+    # log.debug(f'Personas eliminadas: {personas_eliminadas}')
+
+    # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
     for persona in personas:
         log.debug(persona)
