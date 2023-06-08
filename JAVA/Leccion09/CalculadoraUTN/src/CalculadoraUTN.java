66258f362f4492138bc6c95d7c26be855e7ba470
NicoBalbuena <112787278+NicoBalbuena@users.noreply.github.com>
2023-06-08 01:01:37 -0300

    Ticket#324
    Se realiza el menu de la calculadora

--- JAVA/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ JAVA/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
 public class CalculadoraUTN {
-
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("******* Aplicacion Calculadora *******");
-        System.out.print("Digite el valor para el operano1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite el valor para el operano2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resutado = operando1 + operando2;
-        System.out.println("resultado = " + resutado);
+        //Mostramos el menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.println("Operacion a realizar? ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if(operacion >= 1 && operacion <=4){
+            System.out.print("Digite el valor para el operando1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Digite el valor para el operando2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+
+        }
+
     }
 }
