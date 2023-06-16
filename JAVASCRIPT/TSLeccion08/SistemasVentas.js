34d261ad083bd80aac8d2e10a09efe0818c37ba5
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2023-06-16 19:12:27 -0300

    Update SistemasVentas.js
    Correccion creacion de productos

--- JAVASCRIPT/TSLeccion08/SistemasVentas.js
+++ JAVASCRIPT/TSLeccion08/SistemasVentas.js
@@ -61,11 +61,6 @@ class Orden{
    
 }
 
-let producto1 = new Producto('Pantalón', 200);
-let producto2 = new Producto('Camisa', 150);
-console.log(producto1.toString());
-console.log(producto2.toString());
-
     producto1 = new Producto("TV",2500);
     producto2 = new Producto("SmarthPhone",26000);
     console.log(producto1.toString());
