127e3d51dde08e1c88559fb0259767cc17c1d179
Alejo Gutierrez <alejohgutierrez@hotmail.com>
2023-06-18 17:59:03 -0300

    JAVA- Creamos el Menu#370

---
+++ JAVA/Leccion11/ListarPersonas/src/ListadoPersonasApp.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner entrada = new Scanner(System.in);
+        //Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        //Empezamos con el menu
+        var salir = false;
+        while(!salir){
+            mostrarMenu();
+            System.out.println();
+        }//Fin del ciclo while
+
+    }//Fin metodod main
+
+
+    private static void mostrarMenu(){
+        //mostramos las opciones
+        System.out.print("""
+                         ******* Listado de Personas *******
+                         1. Agregar 
+                         2. Listar
+                         3. Salir
+                           """);
+        System.out.print("Digite una de las opciones: ");
+    }
+
+}
