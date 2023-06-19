aa5003f53b67a13eca754e5d23a0971115a57975
Juan Carlos Gutierrez <DiosJuanVero2015@gmail.com>
2023-06-18 21:22:18 -0300

    JAVA - Agregamos el try catch y el método ejecutarOperación() Incorporamos lambda#371

--- JAVA/Leccion11/ListarPersonas/src/ListadoPersonasApp.java
+++ JAVA/Leccion11/ListarPersonas/src/ListadoPersonasApp.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 
 public class ListadoPersonasApp {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Scanner entrada = new Scanner(System.in);
@@ -11,15 +12,19 @@ public class ListadoPersonasApp {
         List<Persona> personas = new ArrayList<>();
         //Empezamos con el menu
         var salir = false;
-        while(!salir){
+        while (!salir) {
             mostrarMenu();
+            try {
+                salir = ejecutarOperacion(entrada, personas);
+            } catch (Exception e) {
+                System.out.println("Ocurrio un Error: " + e.getMessage());
+            }
             System.out.println();
         }//Fin del ciclo while
 
     }//Fin metodod main
 
-
-    private static void mostrarMenu(){
+    private static void mostrarMenu() {
         //mostramos las opciones
         System.out.print("""
                          ******* Listado de Personas *******
@@ -28,6 +33,32 @@ public class ListadoPersonasApp {
                          3. Salir
                            """);
         System.out.print("Digite una de las opciones: ");
-    }
+    }//Fin metodo mostrarMenu
 
-}
+    private static bollean ejecutarOperacion(Scanner entrada, List<Persona> personas) {
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        // revisamos la opcion digitada a travez de un Switch
+        switch (opcion) {
+            case 1 -> {//agregar una persona a la lista
+                System.out.print("Digite el nombre: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el telefono: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el Correo");
+                var email = entrada.nextLine();
+                // creammos el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                //Agregamos la persona a la lista
+                personas.add(persona);
+                System.out.println("La Lista tiene : " + personas.size() + " elementos");
+            }//Fin caso 1
+            case 2 -> {//Listar a las personas
+                System.out.println("Listado de personas: ");
+                //mejoras con lambda y el metodo de referencia
+                //personas.forEach((persona)-> System.out.println(persona));
+                personas.forEach(System.out::println);
+            }
+        }
+    }//fin del metodo ejecutarOperacion
+}//Fin de la clase ListadoPersonasApp
