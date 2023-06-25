a7000d3ff20a6fad7a440abfeb5a807576eceaf5
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-06-25 20:36:40 -0300

    Correciones UI
    Se agregaron un par de arreglos a la clase UI.

--- PYTHON/TrabajoFinal3Semestre/GestionBiblioteca/ui/ui.py
+++ PYTHON/TrabajoFinal3Semestre/GestionBiblioteca/ui/ui.py
@@ -1,67 +1,67 @@
 def mostrar_menu_socio():
-    print("************************************")
-    print('             ABM SOCIO              ')
-    print("************************************")
+    print("*" * 50)
+    print('ABM SOCIO'.center(50, ' '))
+    print("*" * 50)
     print('1.--> Agregar Socio')
     print('2.--> Listar Socios')
     print('3.--> Eliminar un socio')
     print('4.--> Modificar un socio')
     print('5.--> Volver')
-    print("************************************")
+    print("*" * 50)
 
 
 def mostrar_menu_autor():
-    print("************************************")
-    print('             ABM AUTOR              ')
-    print("************************************")
+    print("*" * 50)
+    print('ABM AUTOR'.center(50, ' '))
+    print("*" * 50)
     print("1.--> Agregar autor")
     print('2.--> Listar autores')
     print('3.--> Eliminar un autor')
     print('4.--> Modificar un autor')
     print('5.--> Volver')
-    print("************************************")
+    print("*" * 50)
 
 
 def mostrar_menu_libro():
-    print("************************************")
-    print('            ABM de Libro            ')
-    print("************************************")
+    print("*" * 50)
+    print('ABM de Libro'.center(50, ' '))
+    print("*" * 50)
     print("1.--> Agregar un libro")
     print('2.--> Listar libros')
     print('3.--> Eliminar un libro')
     print('4.--> Modificar un libro')
     print('5.--> Agregar un libro existente')
     print('6.--> Volver')
-    print("************************************")
+    print("*" * 50)
 
 
 def mostrar_menu_principal_abm1():
-    print("************************************")
-    print('         GESTION BIBLIOTECA         ')
-    print("************************************")
+    print("*" * 50)
+    print('ABM de Libro'.center(50, ' '))
+    print("*" * 50)
     print("1.--> ABM SOCIO")
     print('2.--> ABM AUTOR')
     print('3.--> ABM LIBROS')
     print('4.--> Volver')
-    print("************************************")
+    print("*" * 50)
 
 
 def mostrar_menu_principal():
-    print("*******************************************")
-    print('            GESTION BIBLIOTECA             ')
-    print("*******************************************")
+    print("*" * 50)
+    print(' GESTION BIBLIOTECA'.center(50, ' '))
+    print("*" * 50)
     print("1.--> Administración de Socio, Autor, Libro")
     print("2.--> Retiro/Devolución libros")
     print('3.--> Salir')
-    print("*******************************************")
+    print("*" * 50)
 
 
 def mostrar_menu_solicitud():
-    print("************************************")
-    print('         GESTION BIBLIOTECA         ')
-    print("************************************")
+    print("*" * 50)
+    print(' SOLICITUD/DEVOLUCION DE LIBROS'.center(50, ' '))
+    print("*" * 50)
     print("1.--> Solicitud de libro")
     print("2.--> Devolución libros")
     print('3.--> VOLVER')
-    print("************************************")
+    print("*" * 50)
 
